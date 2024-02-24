d574673fdda07826adf77630239924278b9aa80c
dilanur gider <dilanur.gider0@gmail.com>
2024-02-24 11:48:02 +0300

    repositioning icons on bottom app bar add fruitsdatabase add hesabim.xml

--- app/src/main/java/com/example/tedarikcepte/FruitsDatabase.kt
+++ app/src/main/java/com/example/tedarikcepte/FruitsDatabase.kt
@@ -1,4 +1,15 @@
 package com.example.tedarikcepte
 
-class FruitsDatabase {
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+
+
+val database_name = "TedarikCepte"
+val table_name = "Fruits"
+
+val col_id = "id"
+val col_price = "fiyat"
+val col_number = "adet"
+
+class FruitsDatabase (var context: Context): SQLiteOpenHelper(context, database_name, null, 1) {
 }
